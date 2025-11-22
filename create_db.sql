249ffcbf08acc8c2007753817bcd1dcf897106ba
fjama002 <fjama002@gold.ac.uk>
2025-11-22 23:47:32 +0000

    update sql dummy data

--- create_db.sql
+++ create_db.sql
@@ -11,6 +11,15 @@ CREATE TABLE IF NOT EXISTS books (
     price  DECIMAL(5, 2),
     PRIMARY KEY(id));
 
+CREATE TABLE IF NOT EXISTS user_details (
+    id     INT AUTO_INCREMENT,
+    firstname   VARCHAR(50),
+    lastname  VARCHAR(50),
+    username VARCHAR(50),
+    email VARCHAR(100),
+    hashedPassword VARCHAR(100),
+    PRIMARY KEY(id));
+
 # Create the application user
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON berties_books.* TO ' berties_books_app'@'localhost';
--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,5 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25), ('Brave New World', 25.00), ('Animal Farm', 12.99);
+
+INSERT INTO user_details(firstname, lastname, username, email, hashedPassword) VALUES ("sarah", "james", "sarahjames", "sarahjames@gmail.com", ?);
