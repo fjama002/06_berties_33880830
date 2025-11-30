6ef3bd77b8af824d24993a3e67de6bdeedd25611
fjama002 <fjama002@gold.ac.uk>
2025-11-30 12:04:14 +0000

    input validation

--- routes/books.js
+++ routes/books.js
@@ -4,19 +4,26 @@ const router = express.Router();
 
 const redirectLogin = (req, res, next) => {
   if (!req.session.userId) {
-    res.redirect('../users/login') // redirect to the login page
+    res.redirect("../users/login"); // redirect to the login page
   } else {
     next(); // move to the next middleware function
   }
-}
+};
 
 router.get("/search", function (req, res, next) {
   res.render("search.ejs");
 });
 
 router.get("/search_result", function (req, res, next) {
-  //searching in the database
-  res.send("You searched for: " + req.query.keyword);
+  [check("keyword").trim().notEmpty().withMessage("Search input required")],
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.render("search.ejs", { errors: errors.array() });
+      }
+      //searching in the database
+      res.send("You searched for: " + req.query.keyword);
+    };
 });
 
 router.get("/list", redirectLogin, function (req, res, next) {
@@ -32,7 +39,7 @@ router.get("/list", redirectLogin, function (req, res, next) {
 
 router.get("/addbook", function (req, res, next) {
   res.render("addbook.ejs");
-})
+});
 
 router.post("/bookadded", function (req, res, next) {
   // saving data in database
@@ -45,14 +52,14 @@ router.post("/bookadded", function (req, res, next) {
     } else
       res.send(
         "This book is added to the database, name: " +
-        req.body.name +
-        ", at price: " +
-        req.body.price
+          req.body.name +
+          ", at price: " +
+          req.body.price
       );
   });
 });
 
-router.get('/bargainbooks', redirectLogin, function (req, res) {
+router.get("/bargainbooks", redirectLogin, function (req, res) {
   router.get("/bargainbooks", function (req, res, next) {
     // retrieve data in database
     let sqlquery = "SELECT name, price FROM books WHERE price<20;";
@@ -63,7 +70,7 @@ router.get('/bargainbooks', redirectLogin, function (req, res) {
         next(err);
       } else res.send("Name: " + req.body.name + ", price " + req.body.price);
     });
-  })
+  });
 });
 
 // Export the router object so index.js can access it
--- routes/users.js
+++ routes/users.js
@@ -3,80 +3,100 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
+const { check, validationResult } = require("express-validator");
+
 const redirectLogin = (req, res, next) => {
   if (!req.session.userId) {
-    res.redirect('../users/login') // redirect to the login page
+    res.redirect("../users/login"); // redirect to the login page
   } else {
     next(); // move to the next middleware function
   }
-}
+};
 
 router.get("/register", function (req, res, next) {
   res.render("register.ejs");
 });
 
-router.post("/registered", function (req, res, next) {
-  const saltRounds = 10;
-  const plainPassword = req.body.password;
+router.post("/registered");
+{
+  [
+    check("username").trim().notEmpty().withMessage("Username required"),
+    check("password").trim().notEmpty().withMessage("Password required"),
+  ],
+    [check("email").isEmail(), check("username").isLength({ min: 5, max: 20 })],
+    function (req, res, next) {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.render("./register");
+      } else {
+        const saltRounds = 10;
+        const plainPassword = req.body.password;
 
-  bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
-    // Store hashed password in your database.
-    // saving data in database
-    let sqlquery =
-      "INSERT INTO user_details (firstname, lastname, username, email, hashedPassword) VALUES (?,?,?,?,?)";
-    // execute sql query
-    let newrecord = [
-      req.body.firstname,
-      req.body.lastname,
-      req.body.username,
-      req.body.email,
-      hashedPassword,
-    ];
+        bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+          // Store hashed password in your database.
+          // saving data in database
+          let sqlquery =
+            "INSERT INTO user_details (firstname, lastname, username, email, hashedPassword) VALUES (?,?,?,?,?)";
+          // execute sql query
+          let newrecord = [
+            req.body.firstname,
+            req.body.lastname,
+            req.body.username,
+            req.body.email,
+            hashedPassword,
+          ];
 
-    db.query(sqlquery, newrecord, (err, result) => {
-      if (err) {
-        next(err);
-      } else
-        result =
-          "Hello " +
-          req.body.firstname +
-          " " +
-          req.body.lastname +
-          " you are now registered!  We will send an email to you at " +
-          req.body.email;
-      result +=
-        "Your password is: " +
-        req.body.password +
-        " and your hashed password is: " +
-        hashedPassword;
+          db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+              next(err);
+            } else
+              result =
+                "Hello " +
+                req.body.firstname +
+                " " +
+                req.body.lastname +
+                " you are now registered!  We will send an email to you at " +
+                req.body.email;
+            result +=
+              "Your password is: " +
+              req.body.password +
+              " and your hashed password is: " +
+              hashedPassword;
 
-      res.send(result);
-    });
-  });
-});
-
-router.get('/list', redirectLogin, function (req, res) {
-  router.get("/list", function (req, res, next) {
-    let sqlquery = "SELECT username FROM user_details"; // query database to get all the users
-    // execute sql query
-    db.query(sqlquery, (err, result) => {
-      if (err) {
-        next(err);
+            res.send(result);
+          });
+        });
       }
-      res.render("users.ejs", { availableUsers: result });
-    });
-  })
+    };
+}
+
+router.get("/list", redirectLogin, function (req, res) {
+  let sqlquery = "SELECT username FROM user_details"; // query database to get all the users
+  // execute sql query
+  db.query(sqlquery, (err, result) => {
+    if (err) {
+      next(err);
+    }
+    res.render("users.ejs", { availableUsers: result });
+  });
 });
 
 router.get("/login", function (req, res, next) {
   res.render("login.ejs");
 });
 
-router.get("/loggedin", function (req, res, next) {
-  res.send("Login successful!")
-  // Save user session here, when login is successful
-  req.session.userId = req.body.username;
-});
+router.get(
+  "/loggedin",
+  [
+    check("username").notEmpty().withMessage("Username required"),
+    check("password").notEmpty().withMessage("Password required"),
+  ],
+  function (req, res, next) {
+    // Save user session here, when login is successful
+    req.session.userId = req.body.username;
+    res.send("Login successful!");
+  }
+);
 
 // Export the router object so index.js can access it
 module.exports = router;
