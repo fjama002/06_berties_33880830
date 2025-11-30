e1a894e8f4af69e2ad6fad52185b75ce6c2b3338
fjama002 <fjama002@gold.ac.uk>
2025-11-30 10:41:03 +0000

    debug

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 // Import express and ejs
 var express = require("express");
-var session = require ('express-session')
+var session = require("express-session");
 var ejs = require("ejs");
 const path = require("path");
 var mysql = require("mysql2");
-require('dotenv').config();
 
 // Create the express application object
 const app = express();
 const port = process.env.PORT || 8000;
 // Tell Express that we want to use EJS as the templating engine
 app.set("view engine", "ejs");
-
 
 // Set up the body parser
 app.use(express.urlencoded({ extended: true }));
@@ -20,26 +18,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Create a session
-app.use(session({
-    secret: 'somerandomstuff',
+app.use(
+  session({
+    secret: "somerandomstuff",
     resave: false,
     saveUninitialized: false,
     cookie: {
-        expires: 600000
-    }
-}))
+      expires: 600000,
+    },
+  })
+);
 
 // Define our application-specific data
 app.locals.shopData = { shopName: "Bertie's Books" };
 
 // Define the database connection pool
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "berties_books_app",
+  password: "qwertyuiop",
+  database: "berties_books",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-global.db = db
+global.db = db;
 
 // Load the route handlers
 const mainRoutes = require("./routes/main");
