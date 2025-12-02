2e3f1e66d8ab9156d772657f500d959c748a8f68
fjama002 <fjama002@gold.ac.uk>
2025-12-02 19:48:57 +0000

    weather

--- routes/weather.js
+++ routes/weather.js
@@ -1,36 +1,37 @@
-const request = require('request')
+const express = require("express");
+const router = express.Router();
+const request = require('request');
 
-router.get("/weather", function (req, res, next) {
+router.get("/", function (req, res, next) {
     let apiKey = '8a584df7816283e79b4c844d1bc9ba73'
     let city = 'london'
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
     request(url, function (err, response, body) {
-        if (err) {
-            next(err)
-        } else {
-            //res.send(body)
-            var weather = JSON.parse(body)
-            var wmsg = 'It is ' + weather.main.temp +
-                ' degrees in ' + weather.name +
-                '! <br> The humidity now is: ' +
-                weather.main.humidity;
-            res.send(wmsg);
+        // if (err) {
+        //     next(err)
+        // } else {
+        //     var weather = JSON.parse(body)
+        //     var wmsg = 'It is ' + weather.main.temp +
+        //         ' degrees in ' + weather.name +
+        //         '! <br> The humidity now is: ' +
+        //         weather.main.humidity;
+        //     res.send(wmsg);
 
-            var weather = JSON.parse(body)
-            if (weather !== undefined && weather.main !== undefined) {
-                var wmsg = 'It is ' + weather.main.temp +
-                    ' degrees in ' + weather.name +
-                    '! <br> The humidity now is: ' +
-                    weather.main.humidity;
-                res.send(wmsg);
-            }
-            else {
-                res.send("No data found");
-            }
-        }
+        //     var weather = JSON.parse(body)
+        //     if (weather !== undefined && weather.main !== undefined) {
+        //         var wmsg = 'It is ' + weather.main.temp +
+        //             ' degrees in ' + weather.name +
+        //             '! <br> The humidity now is: ' +
+        //             weather.main.humidity;
+        //         res.send(wmsg);
+        //     }
+        //     else {
+        //         res.send("No data found");
+        //     }
+        // } 
     });
-    //res.render("weather.ejs");
+    res.render("weather.ejs");
 });
 
 // Export the router object so index.js can access it
