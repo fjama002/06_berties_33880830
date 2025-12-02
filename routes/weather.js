4ec4532dda43de76d99f1d1078d91cbfb21ca9f9
fjama002 <fjama002@gold.ac.uk>
2025-12-02 19:53:35 +0000

    weather

--- routes/weather.js
+++ routes/weather.js
@@ -8,28 +8,28 @@ router.get("/", function (req, res, next) {
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
     request(url, function (err, response, body) {
-        // if (err) {
-        //     next(err)
-        // } else {
-        //     var weather = JSON.parse(body)
-        //     var wmsg = 'It is ' + weather.main.temp +
-        //         ' degrees in ' + weather.name +
-        //         '! <br> The humidity now is: ' +
-        //         weather.main.humidity;
-        //     res.send(wmsg);
+        if (err) {
+            next(err)
+        } else {
+            var weather = JSON.parse(body)
+            var wmsg = 'It is ' + weather.main.temp +
+                ' degrees in ' + weather.name +
+                '! <br> The humidity now is: ' +
+                weather.main.humidity;
+            res.send(wmsg);
 
-        //     var weather = JSON.parse(body)
-        //     if (weather !== undefined && weather.main !== undefined) {
-        //         var wmsg = 'It is ' + weather.main.temp +
-        //             ' degrees in ' + weather.name +
-        //             '! <br> The humidity now is: ' +
-        //             weather.main.humidity;
-        //         res.send(wmsg);
-        //     }
-        //     else {
-        //         res.send("No data found");
-        //     }
-        // } 
+            var weather = JSON.parse(body)
+            if (weather !== undefined && weather.main !== undefined) {
+                var wmsg = 'It is ' + weather.main.temp +
+                    ' degrees in ' + weather.name +
+                    '! <br> The humidity now is: ' +
+                    weather.main.humidity;
+                res.send(wmsg);
+            }
+            else {
+                res.send("No data found");
+            }
+        } 
     });
     res.render("weather.ejs");
 });
